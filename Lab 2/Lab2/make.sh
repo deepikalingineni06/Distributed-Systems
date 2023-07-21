d551b813e3f06e88de6f5168ef022185c22a1d88
deepikalingineni06 <47357671+deepikalingineni06@users.noreply.github.com>
2023-07-21 18:40:37 -0500

    Delete Lab 2/Lab2 directory

--- Lab 2/Lab2/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm ./directory_a/*
-rm ./directory_b/*
-
-touch ./directory_a/b.txt ./directory_a/d.gif ./directory_a/e.txt
-touch ./directory_b/a.jpg ./directory_b/c.pdf ./directory_b/e.txt
-
-ls ./directory_*
